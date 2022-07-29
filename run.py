70d16a35ede712036929deb6b000828cff922dc4
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2022-07-29 13:08:51 +0000

    Initial commit, creating variables and conditions for users.

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""Creating variables for the users"""
+user_wins = 0
+computer_wins = 0
+
+"""Getting data from users"""
+def options('rock', 'paper', 'scissors'): 
+while True:
+    user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
+    if user_input == 'q':
+           break
+    
+    if user_input not in options:
+        continue
+"""rock: 0, paper: 1, scissors: 2"""
+random_num = random.randint(0,2)
+computer_pick = options[random_num]
+print("Computer picked", computer_pick + ".")
+
+
+print("Goodbye!")
+
+
