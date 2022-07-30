b58b670decf6a2c02a14823fd367f13685e0f1a1
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2022-07-30 16:20:04 +0000

    Creating if statements for winning

--- run.py
+++ run.py
@@ -1,28 +1,36 @@
 import random
 
-"""Creating variables for the users"""
+"""Creating variables for the users."""
 user_wins = 0
 computer_wins = 0
 
-"""Creating variables and conditions for users"""
-options = ["rock", "paper", "scissors"]: 
+"""Creating the flow process and conditions."""
+options = ["rock", "paper", "scissors"]
 while True:
-    user_input = input('Type Rock/Paper/Scissors or Q to quit: ').lower()
-    if user_input == 'q':
-           break
-    
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
     if user_input not in options:
         continue
-"""rock: 0, paper: 1, scissors: 2"""
-random_num = random.randint(0,2)
+
+random_num = random.randint(0, 2)
 computer_pick = options[random_num]
 print("Computer picked", computer_pick + ".")
 
 """Deciding who won"""
-if user_input == 'rock' and computer_pick == 'scissors'
+if user_input == "rock" and computer_pick == "scissors":
     print("You Won!")
     user_wins += 1
-    continue
+
+elif user_input == "paper" and computer_pick == "rock":
+    print("You Won!")
+    user_wins += 1
+
+elif user_input == "scissors" and computer_pick == "paper":
+    print("You Won!")
+    user_wins += 1
+
+        
 
 
 
